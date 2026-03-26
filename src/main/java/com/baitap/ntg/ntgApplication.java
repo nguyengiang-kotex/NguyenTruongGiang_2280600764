5f446ba6a06335e6fe4e4987e9ea6636ecf4c0fe
nguyengiang-kotex <giangkotex.nt204@gmail.com>
2026-03-26 16:22:29 +0700

    Initial commit cho ntg-app

---
+++ src/main/java/com/baitap/ntg/ntgApplication.java
@@ -0,0 +1,13 @@
+package com.baitap.ntg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ntgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ntgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baitap/ntg/ntgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baitap.ntg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ntgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
